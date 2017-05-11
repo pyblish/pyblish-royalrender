8130d668ced5c8d07672b085da635392a7d72d28
Toke Jepsen <tokejepsen@gmail.com>
2017-05-11 17:46:33 +0200

    Initial working version.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # pyblish-royalrender
-Pyblish for Royal Render
+
+## Setup
+
+The environment variable ```RR_Root``` is required to point to the global installation of Royal Render.
+
+## Usage
+
+To use this extension you need to assign the ```royalrender``` family and have data in the ```royalrenderData``` data member on the instance you want to publish.
+
+```royalrenderData``` is an dictionary that gets convert to RoyalRender's xml submission. Please read more about RoyalRender's xml; http://www.royalrender.de/help8/index.html?rrJobsubmitxmlfile.html.   
+You can also get the xml data from an already submitted job in the rrControl application, by selecting a job and choosing "Jobs" > "Export selected Jobs as .xml".
+
+Exported from rrControl:
+
+```xml
+<RR_Job_File syntax_version="6.0">
+<Job>
+    <Software>     Maya     </Software>
+</Job>
+</RR_Job_File>
+```
+
+Data that generated the above job:
+
+```python
+instance.data["royalrenderData"] = {
+  "Software": "Maya"
+}
+```
---
+++ pyblish_royalrender/plugins/integrate_royalrender.py
@@ -0,0 +1,125 @@
+import os
+from xml.etree.ElementTree import ElementTree, Element, SubElement
+import uuid
+import tempfile
+import platform
+import subprocess
+
+import pyblish.api
+
+
+class PyblishRoyalRenderIntegrate(pyblish.api.ContextPlugin):
+    """ Submits to Royal Render. """
+
+    order = pyblish.api.IntegratorOrder
+    label = "Royal Render"
+
+    def indent(self, elem, level=0):
+        i = "\n" + level * "\t"
+        if len(elem):
+            if not elem.text or not elem.text.strip():
+                elem.text = i + "\t"
+            for e in elem:
+                self.indent(e, level + 1)
+                if not e.tail or not e.tail.strip():
+                    e.tail = i + "\t"
+            if not e.tail or not e.tail.strip():
+                e.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+        return True
+
+    def sub_element(self, r, e, text):
+        sub = SubElement(r, e)
+        if (type(text) == unicode):
+            sub.text = text.encode('utf8')
+        else:
+            sub.text = str(text).decode("utf8")
+        return sub
+
+    def dict_to_elements(self, data, element):
+        for key, value in data.iteritems():
+            if isinstance(value, dict):
+                self.dict_to_elements(
+                    value, self.sub_element(element, key, "")
+                )
+            else:
+                # Sanitize path separators
+                if isinstance(value, basestring):
+                    value = value.replace("\\", "/")
+                    self.log.debug(value)
+
+                self.sub_element(element, key, value)
+
+    def get_arguments(self):
+        rr_root = os.environ["RR_Root"]
+
+        # Windows arguments
+        if platform.system() == "Windows":
+            return [
+                os.path.join(rr_root, "bin", "win", "rrStartLocal.exe"),
+                "rrSubmitterConsole.exe"
+            ]
+
+    def process(self, context):
+
+        # Root element
+        root_element = Element("rrJob_submitFile")
+        root_element.attrib["syntax_version"] = "6.0"
+        self.sub_element(root_element, "DeleteXML", "1")
+
+        # Job element
+        for instance in context:
+
+            # Only consider enabled instances
+            if not instance.data.get("publish", True):
+                continue
+
+            # skipping instance if not part of the family
+            if "royalrender" not in instance.data.get("families", []):
+                msg = "No \"royalrender\" family assigned. "
+                msg += "Skipping \"%s\"." % instance
+                self.log.debug(msg)
+                continue
+
+            job_element = self.sub_element(root_element, "Job", "")
+            self.dict_to_elements(
+                instance.data["royalrenderData"], job_element
+            )
+
+        # Writing to disk
+        tree_element = ElementTree(root_element)
+        self.indent(tree_element.getroot())
+
+        xml_path = os.path.join(
+            tempfile.gettempdir(),
+            "pyblish_royalrender_{0}.xml".format(uuid.uuid4())
+        )
+        with open(xml_path, "w") as f:
+            tree_element.write(f)
+
+        with open(xml_path, "r") as f:
+            self.log.debug(f.read())
+
+        # Submitting to RoyalRender
+        arguments = self.get_arguments()
+        arguments.append(xml_path)
+
+        startupinfo = None
+        if hasattr(subprocess, "STARTF_USESHOWWINDOW"):
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+        proc = subprocess.Popen(
+            arguments,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            startupinfo=startupinfo
+        )
+        proc.stdin.close()
+        proc.stderr.close()
+
+        output = proc.stdout.read()
+        self.log.debug(output)
